6c865d7675717bbe3e9c0198b56e0c5f80459a07
sethherr <seth.william.herr@gmail.com>
2019-04-24 10:31:35 -0700

    wip

--- rufo_format.py
+++ rufo_format.py
@@ -1,53 +1,23 @@
+import os, sublime
 import sublime_plugin
-import sublime
-import subprocess
-from .diff_match_patch import diff_match_patch
-import json
+
 
 class RufoPluginListener(sublime_plugin.EventListener):
   def on_pre_save(self, view):
-    settings = sublime.load_settings('sublime-rufo.sublime-settings')
-    if settings.get('auto_format') == None or settings.get('auto_format') == True:
-      view.run_command('rufo_format')
+    view.run_command("rufo_format")
 
 class RufoFormatCommand(sublime_plugin.TextCommand):
-  def is_enabled(self):
-    caret = self.view.sel()[0].a
-    syntax_name = self.view.scope_name(caret)
-    return "source.ruby" in syntax_name
-
-  def has_redo(self):
-    cmd, args, repeat = self.view.command_history(1)
-    return cmd != ''
-
   def run(self, edit):
-    vsize = self.view.size()
-    region = sublime.Region(0, vsize)
-    src = self.view.substr(region)
     window = self.view.window()
     filename = self.view.file_name()
+    # command = [rufo_cmd, "--filename", filename]
+    # caret = self.view.sel()[0].a
+    # msg = self.view.scope_name(caret)
+    # msg = "'Hello, World!'\n"
 
-    settings = sublime.load_settings('sublime-rufo.sublime-settings')
-    rufo_cmd = settings.get("rufo_cmd") or "rufo"
+    project_data = sublime.Window.project_data()
 
-    command = [rufo_cmd]
-    if filename != None:
-      command = [rufo_cmd, "--filename", filename]
-
-    with subprocess.Popen(command, stdin = subprocess.PIPE, stdout = subprocess.PIPE) as proc:
-      proc.stdin.write(bytes(src, 'UTF-8'))
-      proc.stdin.close()
-      output = proc.stdout.read().decode('UTF-8')
-      exit = proc.wait()
-
-    pos = 0
-    if exit == 3:
-      if not self.has_redo():
-        for op, text in diff_match_patch().diff_main(src, output):
-          if op == diff_match_patch.DIFF_DELETE:
-            self.view.erase(edit, sublime.Region(pos, pos + len(text)))
-          if op == diff_match_patch.DIFF_INSERT:
-            self.view.insert(edit, pos, text)
-            pos += len(text)
-          if op == diff_match_patch.DIFF_EQUAL:
-            pos += len(text)
+    # sublime.error_message(msg)
+    # os.chdir()
+    print(msg)
+    # self.console.log(sublime.Window.project_data())
